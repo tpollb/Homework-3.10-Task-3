869c1e6203308557107f9ba378cb4636b8f3a2f9
Сергей Усков <q3mydoom@hotmail.com>
2021-10-26 13:54:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework_3._10_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int reminder;
+            int i;
+
+            Console.WriteLine("Enter number");
+            number = int.Parse(Console.ReadLine());
+
+            for (i = 2; i < number; i++)
+            {
+                reminder = number % i;
+                    if (reminder == 0)
+                    {
+                        Console.WriteLine($"Number {number} is NOT prime");
+                        break;
+                    } else if (i == number -1)
+                        {
+                            Console.WriteLine($"Number {number} is prime");
+                        }
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
